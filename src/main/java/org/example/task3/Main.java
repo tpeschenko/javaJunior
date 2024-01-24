774f7dcf0fb6c7bbfef690da113d0775fa60e549
Taras <taras.peschenko.90@mail.ru>
2024-01-24 04:34:10 +0300

    add Task3

--- src/main/java/org/example/task3/Main.java
+++ src/main/java/org/example/task3/Main.java
@@ -1,2 +1,42 @@
-package org.example.task3;public class Main {
+package org.example.task3;
+
+ /* Разработайте класс Student с полями String name, int age, transient double GPA (средний балл).
+Обеспечьте поддержку сериализации для этого класса.
+Создайте объект класса Student и инициализируйте его данными.
+Сериализуйте этот объект в файл.
+Десериализуйте объект обратно в программу из файла.
+Выведите все поля объекта, включая GPA, и ответьте на вопрос,
+почему значение GPA не было сохранено/восстановлено.
+*/
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Student student = new Student("Василий", 22, 3.1);
+
+        try(FileOutputStream fileOut = new FileOutputStream("file.bin");
+            ObjectOutputStream out = new ObjectOutputStream(fileOut)) {
+            out.writeObject(student);
+            System.out.println("Объект сериализован.");
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+
+        try(FileInputStream fileInput = new FileInputStream("file.bin");
+            ObjectInputStream input = new ObjectInputStream(fileInput)) {
+            Student student2;
+            student2 = (Student)input.readObject();
+            System.out.println("Объект десериализован");
+            System.out.println(student2);
+
+        } catch (ClassNotFoundException | IOException e) {
+            e.printStackTrace();
+        }
+
+
+
+    }
 }
--- src/main/java/org/example/task3/Student.java
+++ src/main/java/org/example/task3/Student.java
@@ -1,2 +1,24 @@
-package org.example.task3;public class Student {
+package org.example.task3;
+
+import java.io.Serializable;
+
+public class Student implements Serializable {
+    private String name;
+    public int age;
+    transient double GPA;
+
+    public Student(String name, int age, double GPA) {
+        this.name = name;
+        this.age = age;
+        this.GPA = GPA;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", GPA=" + GPA +
+                '}';
+    }
 }
